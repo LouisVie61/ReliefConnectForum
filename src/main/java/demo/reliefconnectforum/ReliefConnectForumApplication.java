3329d11b691b313d09f14478f1b44b3477d82a4c
LouisVie61 <tantrieucao@gmail.com>
2025-10-10 12:19:23 +0700

    feat: add codebase

---
+++ src/main/java/demo/reliefconnectforum/ReliefConnectForumApplication.java
@@ -0,0 +1,13 @@
+package demo.reliefconnectforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliefConnectForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReliefConnectForumApplication.class, args);
+    }
+
+}
