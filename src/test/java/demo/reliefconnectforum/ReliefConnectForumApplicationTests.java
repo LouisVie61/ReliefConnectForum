4d1eb6910ee9455dd67c92b5c6be40b838618785
LouisVie61 <tantrieucao@gmail.com>
2025-10-10 12:19:23 +0700

    feat: add codebase

---
+++ src/test/java/demo/reliefconnectforum/ReliefConnectForumApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.reliefconnectforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliefConnectForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
