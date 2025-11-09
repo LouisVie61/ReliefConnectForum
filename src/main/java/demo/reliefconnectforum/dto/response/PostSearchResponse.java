599228af6a7330c9805a83ff5a338267abec300f
LouisVie61 <tantrieucao@gmail.com>
2025-11-09 10:03:39 +0700

    feat: prepare service layer

--- src/main/java/demo/reliefconnectforum/dto/response/PostSearchResponse.java
+++ src/main/java/demo/reliefconnectforum/dto/response/PostSearchResponse.java
@@ -1,4 +1,25 @@
 package demo.reliefconnectforum.dto.response;
 
+import demo.reliefconnectforum.Enum.PostType;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostSearchResponse {
+    private String id;
+    private String title;
+    private String content;
+    private PostType postType;
+    private String location;
+    private String authorId;
+    private String authorUsername;
+    private BigDecimal targetAmount;
+    private BigDecimal currentAmount;
+    private LocalDateTime createdAt;
 }
--- src/main/java/demo/reliefconnectforum/service/core/PostDocService.java
+++ src/main/java/demo/reliefconnectforum/service/core/PostDocService.java
@@ -1,4 +1,13 @@
 package demo.reliefconnectforum.service.core;
 
+import demo.reliefconnectforum.dto.response.PostSearchResponse;
+import demo.reliefconnectforum.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface PostDocService {
+    void indexPost(Post post);
+    void deletePost(String postId);
 }
